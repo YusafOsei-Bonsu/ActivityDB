336bc001465dc9141e2c8aa19569a3a5852673a0
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2020-01-14 17:12:11 +0000

    Comments

--- knex/migrations/20200114161608_placeid_in_activities.js
+++ knex/migrations/20200114161608_placeid_in_activities.js
@@ -1,10 +1,11 @@
-
+// Appends the foreign key (id of places) to the Activities table
 exports.up = function(knex) {
   return knex.schema.table('activities', function(table) {
       table.integer('p_id').unsigned().index().references('placeid').inTable('places')
   })
 };
 
+// Drops the foreign key (id of places) column from the Activities table
 exports.down = function(knex) {
   return knex.schema.table('activities', function(table) {
     table.dropColumn('p_id');
