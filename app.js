c0f43b4d46213bc3b4cbaba855f5ac52ad6e11d9
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2020-01-14 17:43:25 +0000

    Routes so far

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const PORT = process.env.PORT || 3001;
 const knex = require('./knex/knex.js');
 const app = express();
 
-app.get('/tasks', (req, res) => {
-  // use the knex variable above to create dynamic queries
+app.get('/', (req, res) => res.send('Hello world'));
+
+// Shows all activities available in the city
+app.get('/activities', (req, res) => knex.from('activities').select('*').then(response => res.send(response)));
+
+// Shows all places in the city
+app.get('/places', (req, res) => knex.from('places').select('*').then(response => res.send(response)));
+
+// Add a new activity into the database
+app.post('/newActivity', (req, res) => {
+  
 });
 
-app.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
