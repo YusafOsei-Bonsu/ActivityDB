5c958744b304f2678a55befee2f293038c3cdc39
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2020-01-14 17:52:27 +0000

    Route that adds a new activity

--- app.js
+++ app.js
@@ -12,8 +12,13 @@ app.get('/activities', (req, res) => knex.from('activities').select('*').then(re
 app.get('/places', (req, res) => knex.from('places').select('*').then(response => res.send(response)));
 
 // Add a new activity into the database
-app.post('/newActivity', (req, res) => {
-  
+app.post('/activity', (req, res) => {
+  let id=req.query.id;
+  let name=req.query.name;
+  let fun=req.query.fun;
+  let price=req.query.price;
+
+  knex('activities').insert({ activityid: id, name: name, fun: fun, price: price }).then(res.redirect('/activities'));
 });
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
