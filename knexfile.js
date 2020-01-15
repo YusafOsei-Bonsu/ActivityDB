029d74ec9707de32d7ce526d635b39f08cc1d9f5
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2020-01-15 11:32:20 +0000

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,36 @@
 // Update with your config settings.
+const env = require('dotenv').config().parsed;
 
 module.exports = {
+  test: {
+    client: 'pg',
+    connection: {
+      host: '127.0.0.1',
+      user: env.yusaf_username,
+      password: env.yusaf_pwd,
+      database: 'mocha_chai_tv_shows_test',
+      charset: 'utf8'
+    },
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/test'
+    }
+  },
+
+  // test: {
+  //   client: 'pg',
+  //   connection: 'postgres://localhost/project',
+  //   migrations: {
+  //     directory: __dirname + '/db/migrations'
+  //   },
+  //   seeds: {
+  //     directory: __dirname + '/db/seeds/test'
+  //   }
+  // },
+
+
     development: {
       client: 'pg',
       connection: {
