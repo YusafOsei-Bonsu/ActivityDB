b50e050e9becf9d990a9ff49a988c62f056d49e7
jadesuzarte <57177552+jadesuzarte@users.noreply.github.com>
2020-01-15 17:05:50 +0000

    add /places test

--- test/API.test.js
+++ test/API.test.js
@@ -25,10 +25,32 @@ describe('API routes', () => {
                 res.body[0].fun.should.equal(8);
                 res.body[0].should.have.property('price');
                 res.body[0].price.should.equal(120);
-                res.body[0].should.have.property('p_id');
-                res.body[0].p_id.should.equal(1);
+                // res.body[0].should.have.property('p_id');
+                // res.body[0].p_id.should.equal('null');
                 done();
             });
         });
     });
+
+    describe('GET /places', () => {
+        it('should return all places', (done) => {
+        chai.request(server)
+        .get('/places')
+        .end(function(err, res) {
+            res.should.have.status(200);
+            res.should.be.json;
+            res.body.should.be.a('array');
+            res.body.length.should.equal(3);
+            res.body[0].should.have.property('placename');
+            res.body[0].placename.should.equal('London Eye');
+            res.body[0].should.have.property('streetaddress');
+            res.body[0].streetaddress.should.equal('Riverside Building, County Hall, Westminster Bridge Road');
+            res.body[0].should.have.property('county');
+            res.body[0].county.should.equal('London');
+            res.body[0].should.have.property('postcode');
+            res.body[0].postcode.should.equal('SE17PB');
+            done();
+           })
+        })
+    } )
 });
