c727a3d6dff2be91e88743a61b3f80bac224dd18
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2020-01-15 14:57:05 +0000

    Provided each location with their actual details

--- knex/seeds/initial_places.js
+++ knex/seeds/initial_places.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('places').insert([
-        {placeid: 1, placename: 'London Eye', streetaddress: 'London eye way', county: 'london', postcode: 'AH78UI'},
-        {placeid: 2, placename: 'Buckingham Palace', streetaddress: 'Oxford st', county: 'london', postcode: 'AH78HJ'},
-        {placeid: 3, placename: 'London Aquarium', streetaddress: 'Riverside Building', county: 'london', postcode: 'AY68UI'}
+        {placeid: 1, placename: 'London Eye', streetaddress: 'Riverside Building, County Hall, Westminster Bridge Road', county: 'London', postcode: 'SE17PB'},
+        {placeid: 2, placename: 'Bunker 51', streetaddress: '3 Herringham Rd, Greenwich Peninsula', county: 'London', postcode: 'SE78NJ'},
+        {placeid: 3, placename: 'Natural History Museum', streetaddress: 'Cromwell Rd, South Kensington', county: 'London', postcode: 'SW7 5BD'}
       ]);
     });
 };
